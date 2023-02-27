0829815f5467cd9bf93641ef60717203d9725903
Felipe Saff <luizsaff@tutanota.com>
2023-02-26 23:50:00 -0400

    first commit

---
+++ src/components/Header/header.styled.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const HeaderComponent = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: #6991ff;
+    padding: 15px 10px;
+    border-radius: 10px;
+
+    span {
+        display: flex;
+    }
+`
+export const InputTitle = styled.input`
+    border: 1px solid #fefefe;
+    border-radius: 5px;
+    padding: 5px 10px;
+    outline: none;
+    color: #5f5f5f;
+`
+
+export const CategoryComponent = styled.select`
+    border: 1px solid #fefefe;
+    border-radius: 5px;
+    padding: 5px 10px;
+    outline: none;
+    color: #5f5f5f;
+`
