7cc7782d0445c43c4a461c86b630118467ca6d73
Felipe Saff <luizsaff@tutanota.com>
2023-02-27 12:01:03 -0400

    Updates

---
+++ pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/Header"
+import { TodoList } from "../../components/TodoList"
+
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+            <TodoList />
+        </div>
+    )
+}
